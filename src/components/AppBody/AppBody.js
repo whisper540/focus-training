e0de79751ecc837454f8587c7fc40b7c483e15d5
wushidong <wushidong@66nao.com>
2019-01-04 17:04:00 +0800

    20190104 17:03 commit

--- src/components/AppBody/AppBody.js
+++ src/components/AppBody/AppBody.js
@@ -19,14 +19,77 @@ const AppBody = () => {
                     <input type="search" className="search-input" placeholder="搜索"/>
                     <button className="create-btn">新建训练</button>
                 </div>
-                <div>
-                    B
+                <div className="train-list-container">
+                    <dl className="train-list">
+                        <dt>
+                            <span>状态</span>
+                            <span>人数</span>
+                            <span>创建时间</span>
+                            <span>进度</span>
+                            <span>备注</span>
+                            <span>操作</span>
+                        </dt>
+                        <dd>
+                            <span>草稿</span>
+                            <span>9</span>
+                            <span>2019.01.04 10:10</span>
+                            <span><progress></progress></span>
+                            <span>言语1部</span>
+                            <span>查看</span>
+                        </dd>
+                        <dd>
+                            <span>草稿</span>
+                            <span>9</span>
+                            <span>2019.01.04 10:10</span>
+                            <span><progress></progress></span>
+                            <span>言语1部</span>
+                            <span>查看</span>
+                        </dd>
+                        <dd>
+                            <span>草稿</span>
+                            <span>9</span>
+                            <span>2019.01.04 10:10</span>
+                            <span><progress></progress></span>
+                            <span>言语1部</span>
+                            <span>查看 <i>删除</i></span>
+                        </dd>
+                        <dd>
+                            <span>草稿</span>
+                            <span>9</span>
+                            <span>2019.01.04 10:10</span>
+                            <span><progress></progress></span>
+                            <span>言语1部</span>
+                            <span>查看</span>
+                        </dd>
+                        <dd>
+                            <span>草稿</span>
+                            <span>9</span>
+                            <span>2019.01.04 10:10</span>
+                            <span><progress></progress></span>
+                            <span>言语1部</span>
+                            <span>查看</span>
+                        </dd>
+                        <dd>
+                            <span>草稿</span>
+                            <span>9</span>
+                            <span>2019.01.04 10:10</span>
+                            <span><progress></progress></span>
+                            <span>言语1部</span>
+                            <span>查看</span>
+                        </dd>
+                        <dd>
+                            <span>草稿</span>
+                            <span>9</span>
+                            <span>2019.01.04 10:10</span>
+                            <span><progress></progress></span>
+                            <span>言语1部</span>
+                            <span>查看</span>
+                        </dd>
+                    </dl>
                 </div>
-                <div>
-                    C
-                </div>
-                <div>
-                    D
+                <div className="train-page-btn">
+                    <span>下一页</span>
+                    <span>上一页</span>
                 </div>
             </div>
         </div>
